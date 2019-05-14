39b662f3d7b9173bfa5a34c305247ba94a7fd057
Ilya Starokozhov <grachara@gmail.com>
2019-05-14 16:25:38 +0300

    fixes made for 1 deadline

--- component---src-pages-404-js-27e19197dd45b399cdf6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{283:function(e,t,n){"use strict";n.r(t);var a=n(1),u=n.n(a),l=n(315),o=n(312);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-27e19197dd45b399cdf6.js.map
